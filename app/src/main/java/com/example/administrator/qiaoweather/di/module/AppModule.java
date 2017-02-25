ad64cb3d68aee2993105410ed09fb4327a41b2cb
haojunmin <1149598411@qq.com>
2017-02-25 22:35:59 +0800

    第一次修改

--- app/src/main/java/com/example/administrator/qiaoweather/di/module/AppModule.java
+++ app/src/main/java/com/example/administrator/qiaoweather/di/module/AppModule.java
@@ -3,7 +3,10 @@ package com.example.administrator.qiaoweather.di.module;
 
 import com.example.administrator.qiaoweather.App;
 import com.example.administrator.qiaoweather.db.RealmService;
+import com.example.administrator.qiaoweather.http.RetrofitHelper;
+
 import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -29,5 +32,11 @@ public class AppModule {
     @Singleton
     RealmService provideRealmService() {
         return new RealmService();
+    }
+
+    @Provides
+    @Singleton
+    RetrofitHelper provideRetrofitHelper() {
+        return new RetrofitHelper();
     }
 }

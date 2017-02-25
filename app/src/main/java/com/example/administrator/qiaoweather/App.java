2ab75f4011a7d93b0fcb51b0c92dc58fcc94f1f8
haojunmin <1149598411@qq.com>
2017-02-25 22:40:49 +0800

    第一次修改

--- app/src/main/java/com/example/administrator/qiaoweather/App.java
+++ app/src/main/java/com/example/administrator/qiaoweather/App.java
@@ -1,6 +1,8 @@
 package com.example.administrator.qiaoweather;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.example.administrator.qiaoweather.di.componet.AppComponet;
 import com.example.administrator.qiaoweather.di.componet.DaggerAppComponet;
@@ -11,6 +13,11 @@ import com.example.administrator.qiaoweather.di.componet.RxCacheComponet;
 import com.example.administrator.qiaoweather.di.module.AppModule;
 import com.example.administrator.qiaoweather.di.module.ImageModule;
 import com.example.administrator.qiaoweather.di.module.RxCacheModule;
+import com.example.administrator.qiaoweather.util.AppBlockCanaryContext;
+import com.facebook.stetho.Stetho;
+import com.github.moduth.blockcanary.BlockCanary;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -21,6 +28,7 @@ import io.realm.RealmConfiguration;
  */
 
 public class App extends Application {
+    private RefWatcher refWatcher;
     private static App instance;
     private static final String DB_NAME = "weather.realm";
     private static AppComponet mAppComponet;
@@ -36,8 +44,49 @@ public class App extends Application {
         super.onCreate();
         instance = this;
         initRealm();
+        BlockCanary.install(this, new AppBlockCanaryContext()).start();
+        refWatcher = LeakCanary.install(this);
+        //配置Strtho
+        Stetho.initializeWithDefaults(this);
+
     }
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //Android应用打破65K方法数限制的方法
+        MultiDex.install(this);
+    }
+
+    /**
+     * 程序终止的时候运行
+     */
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        if (refWatcher != null) {
+            refWatcher = null;
+        }
+
+        if (instance != null) {
+            instance = null;
+        }
+        if (mAppComponet != null) {
+            mAppComponet = null;
+        }
+        if (mRxCacheComponet != null) {
+            mRxCacheComponet = null;
+        }
+        if (mImageComponet != null) {
+            mImageComponet = null;
+        }
+
+
+    }
+
+    /**
+     * 初始化数据库
+     */
     private void initRealm() {
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder().name(DB_NAME).build();

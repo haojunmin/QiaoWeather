7411f5e140541436d6dd705c073eb3bbeb83574e
haojunmin <1149598411@qq.com>
2017-02-28 23:03:53 +0800

    第二次修改

--- app/src/main/java/com/example/administrator/qiaoweather/db/RealmService.java
+++ app/src/main/java/com/example/administrator/qiaoweather/db/RealmService.java
@@ -1,6 +1,9 @@
 package com.example.administrator.qiaoweather.db;
 
 
+import com.example.administrator.qiaoweather.App;
+import com.example.administrator.qiaoweather.enty.City;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -10,14 +13,38 @@ import io.realm.RealmResults;
 
 public class RealmService {
     private Realm mRealm;
+    App mApp;
 
-    public RealmService() {
-        mRealm = Realm.getDefaultInstance();
+    public RealmService(App app) {
+        this.mApp = app;
     }
 
+    public Realm getmRealm() {
+        mRealm = Realm.getInstance(mApp.getRealmConfiguration());
+        return mRealm;
+    }
+
+    public RealmResults<City> findAllCity() {
+        return mRealm.where(City.class).findAllAsync();
+    }
+
+    public void setmRealm(Realm mRealm) {
+        this.mRealm = mRealm;
+    }
+
+    public void save(final String city) {
+        mRealm.beginTransaction();
+        City p = mRealm.createObject(City.class);
+        p.setName(city);
+        mRealm.commitTransaction();
+        mRealm.close();
+    }
 
     public void closeRealm() {
-        mRealm.close();
+        if (mRealm != null) {
+            mRealm.close();
+        }
+
     }
 
     public interface OnTransactionCallback {

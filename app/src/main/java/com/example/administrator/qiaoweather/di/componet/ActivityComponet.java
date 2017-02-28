5b3537d825f974842103d540ac0def0b58fd1319
haojunmin <1149598411@qq.com>
2017-02-28 23:04:11 +0800

    第二次修改

--- app/src/main/java/com/example/administrator/qiaoweather/di/componet/ActivityComponet.java
+++ app/src/main/java/com/example/administrator/qiaoweather/di/componet/ActivityComponet.java
@@ -2,6 +2,10 @@ package com.example.administrator.qiaoweather.di.componet;
 
 import android.app.Activity;
 
+import com.example.administrator.qiaoweather.AboutActivity;
+import com.example.administrator.qiaoweather.AddCityActivity;
+import com.example.administrator.qiaoweather.CityListActivity;
+import com.example.administrator.qiaoweather.MainActivity;
 import com.example.administrator.qiaoweather.di.ActivityScope;
 import com.example.administrator.qiaoweather.di.module.ActivityModule;
 
@@ -9,10 +13,20 @@ import dagger.Component;
 
 /**
  * Created by Administrator on 2017/2/25.
+ * 可以绑定不同的activity
  */
 
 @ActivityScope
-@Component(modules = ActivityModule.class)
+@Component(dependencies = AppComponet.class, modules = ActivityModule.class)
 public interface ActivityComponet {
-    Activity getActivity();
+    Activity activity();
+
+    void inject(MainActivity mainActivity);
+
+    void inject(CityListActivity cityListActivity);
+
+    void inject(AddCityActivity addCityActivity);
+
+    void inject(AboutActivity aboutActivity);
+
 }
--- app/src/main/java/com/example/administrator/qiaoweather/di/componet/AppComponet.java
+++ app/src/main/java/com/example/administrator/qiaoweather/di/componet/AppComponet.java
@@ -1,9 +1,14 @@
 package com.example.administrator.qiaoweather.di.componet;
 
 import com.example.administrator.qiaoweather.App;
+import com.example.administrator.qiaoweather.CityListActivity;
+import com.example.administrator.qiaoweather.MainActivity;
 import com.example.administrator.qiaoweather.db.RealmService;
+import com.example.administrator.qiaoweather.di.module.ActivityModule;
 import com.example.administrator.qiaoweather.di.module.AppModule;
 import com.example.administrator.qiaoweather.http.RetrofitHelper;
+
+import com.example.administrator.qiaoweather.imageloader.ImageLoader;
 
 import javax.inject.Singleton;
 
@@ -15,9 +20,16 @@ import dagger.Component;
 @Singleton
 @Component(modules = {AppModule.class})
 public interface AppComponet {
+    void inject(App app);
+
+
     App getContext();
 
     RealmService realmService();
 
     RetrofitHelper retrofitHelper();
+
+    ImageLoader imageLoader();
+
+
 }
